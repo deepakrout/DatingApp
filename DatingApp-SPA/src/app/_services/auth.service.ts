324d821cba8b11f0894822072940e0c1ea1b0a30
Deepak Rout <deepakrout@gmail.com>
2019-04-15 19:09:28 -0400

    Commiting after  User list and profile page.

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -2,19 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import {JwtHelperService} from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = `http://localhost:5000/api/auth`;
+  baseUrl =  `${environment.apiUrl}auth/`;    // `http://localhost:5000/api/auth`
   jwtHelper = new JwtHelperService();
   decodedToken: any;
 
   constructor(private http: HttpClient) {}
 
   login(model: any) {
-    return this.http.post(`${this.baseUrl}/login`, model).pipe(
+    return this.http.post(`${this.baseUrl}login`, model).pipe(
       map((response: any) => {
         const user = response;
 
@@ -30,7 +31,7 @@ export class AuthService {
   }
 
   register(model: any) {
-    return this.http.post(`${this.baseUrl}/register`, model);
+    return this.http.post(`${this.baseUrl}register`, model);
   }
 
   loggedIn() {
---
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/user';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+  user: User;
+
+  constructor(private userService: UserService,
+              private alertify: AlertifyService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // this.loadUser();
+    this.route.data.subscribe( data => {
+      this.user = data['user'];
+    });
+  }
+
+
+  loadUser() {
+    this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
+      this.user = user;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+}
