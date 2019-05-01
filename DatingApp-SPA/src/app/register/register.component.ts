9ee3b439f439a3955ea5ad365f1b1153bea1cf26
Deepak Rout <deepakrout@gmail.com>
2019-04-30 22:38:58 -0400

    Commiting after reactive register form

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { BsDatepickerModule, BsDatepickerConfig } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-register',
@@ -11,21 +13,48 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
+  registerForm: FormGroup;
+  bsConfig: Partial<BsDatepickerConfig>;
 
-  constructor(private auth: AuthService, private alertify: AlertifyService) {}
+  constructor(private auth: AuthService,
+              private alertify: AlertifyService,
+              private fb: FormBuilder) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.bsConfig = {
+      containerClass: 'theme-red'
+    }
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+        username: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+        confirmPassword: ['', Validators.required],
+        gender: ['male'],
+        knownAs: ['', Validators.required],
+        dateOfBirth: [null, Validators.required],
+        city: ['', Validators.required],
+        country: ['', Validators.required]
+    }, {validator: this.passwordMatchValidator});
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : { mismatch : true };
+  }
 
   register(): void {
-    console.log(this.model);
-    this.auth.register(this.model).subscribe(() => {
-      console.log(`Registration Success`);
-      this.alertify.success(`Registration Success`);
-    },
-    (error) => {
-      console.log(`Error in registering User`, error);
-      this.alertify.error(`Error in registering User: ${error}`);
-    });
+    // console.log(this.model);
+    // this.auth.register(this.model).subscribe(() => {
+    //   console.log(`Registration Success`);
+    //   this.alertify.success(`Registration Success`);
+    // },
+    // (error) => {
+    //   console.log(`Error in registering User`, error);
+    //   this.alertify.error(`Error in registering User: ${error}`);
+    // });
+    console.log(this.registerForm.value);
   }
 
   cancel(): void {
